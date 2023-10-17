1ede704c21f3bcb770ac3b6a0375543263d091e0
Diogo RBG <diogorbg@gmail.com>
2023-10-16 21:34:10 -0300

    correções

--- scripts/fase.gd
+++ scripts/fase.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 @export var fase: int = 1
 
+# Resetar deve ser a primeira coisa a ser feita, antes que a fase esteja carregada
 func _init():
 	Global.reset()
 
+# Só podemos repassar o valor da fase no evento _ready()
 func _ready():
 	Global.fase = fase
--- scripts/menu.gd
+++ scripts/menu.gd
@@ -8,6 +8,5 @@ func _ready():
 func _on_butJogar_pressed():
 	get_tree().change_scene_to_file("res://scenes/fase1.tscn")
 
-
 func _on_butSair_pressed():
 	get_tree().quit()
